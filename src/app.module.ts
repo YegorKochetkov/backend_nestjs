d945c26d3205508796ca540bc18d9f8e766a6947
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-15 13:15:16 +0300

    feat: add postgres with sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'password',
+      database: 'backend_nestjs',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
