458dfadaafc33d9b3ed5f84b7ea3ff5d1a978727
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-16 00:34:47 +0300

    refactor: remove readme endpoint will implement swagger instead

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { User } from './users/models/user.model';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [
     ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }),
     SequelizeModule.forRoot({
