37f25aa8f6bf823a85bf5ce1337c004d90f3ebc1
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-20 14:50:42 +0300

    feat: set up users controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,24 @@
+import { getModelToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from './models/user.model';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+const usersArray = [
+  {
+    email: 'email',
+    password: 'password',
+  },
+  {
+    email: 'email2',
+    password: 'password2',
+  },
+];
+
+const oneUser = {
+  email: 'email',
+  password: 'password',
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,6 +26,19 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(User),
+          useValue: {
+            findAll: jest.fn(() => usersArray),
+            create: jest.fn(() => oneUser),
+            // findOne: jest.fn(),
+            // remove: jest.fn(),
+            // destroy: jest.fn(() => oneUser),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
