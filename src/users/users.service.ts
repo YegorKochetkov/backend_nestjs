ecf738b8c8506bf7e410da550a17d9b7a65ae535
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-21 21:29:56 +0300

    feat: set role when create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Roles } from '../constants/constants';
+import { RolesService } from '../roles/roles.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './models/user.model';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  constructor(
+    @InjectModel(User) private userRepository: typeof User,
+    private roleService: RolesService,
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
-    return await this.userRepository.create({
+    const user = await this.userRepository.create({
       email: createUserDto.email,
       password: createUserDto.password,
     });
+    const role = await this.roleService.findOne(Roles.user);
+
+    await user.$set('roles', [role.id]);
+
+    return user;
   }
 
   async findAll() {
-    return await this.userRepository.findAll();
+    return await this.userRepository.findAll({
+      include: { all: true },
+    });
   }
 }
