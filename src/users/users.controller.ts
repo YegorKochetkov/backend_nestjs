0c832a847bf012aec673e462096db33d3b33bf5f
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-25 17:52:18 +0300

    refactor: remove create user endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,21 +2,13 @@ import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './models/user.model';
 import { UsersService } from './users.service';
-import {
-  ApiExcludeEndpoint,
-  ApiOperation,
-  ApiParam,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @ApiExcludeEndpoint(true)
-  @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.create(userDto);
   }
