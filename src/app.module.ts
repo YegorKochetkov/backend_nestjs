e0f8d93b86af03fc90477c4e6fb20b74609b60b3
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-13 21:22:00 +0300

    refactor: remove port from sequelize module

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,7 +38,6 @@ if (process.env.NODE_ENV === 'development') {
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
       database: process.env.POSTGRES_DATABASE,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
