634f0af7eeb087258318aff6b1ada636836c4480
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-13 20:27:34 +0300

    feat: add config for postgres add ServeStaticModule for images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 import { AuthModule } from './auth/auth.module';
+import { FilesModule } from './files/files.module';
 import { Post } from './posts/models/post.model';
 import { PostsModule } from './posts/posts.module';
 import { Role } from './roles/models/role.model';
@@ -9,7 +13,18 @@ import { RolesModule } from './roles/roles.module';
 import { UserRoles } from './users/models/user-roles.model';
 import { User } from './users/models/user.model';
 import { UsersModule } from './users/users.module';
-import { FilesModule } from './files/files.module';
+
+dotenv.config();
+
+let dialectOptions: object = {
+  ssl: {
+    require: true,
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  dialectOptions = {};
+}
 
 @Module({
   controllers: [],
@@ -25,7 +40,9 @@ import { FilesModule } from './files/files.module';
       password: process.env.POSTGRES_PASSWORD,
       models: [User, Role, UserRoles, Post],
       autoLoadModels: true,
+      dialectOptions,
     }),
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
     UsersModule,
     RolesModule,
     AuthModule,
